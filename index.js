788e638a8346f2be9b43af7033b79691b048dde6
Milleydev <milleymwongeli@gmail.com>
2023-11-21 15:01:48 +0300

    updated default city as Nairobi

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function search(event) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
+  searchCity("Nairobi");
 }
 
 function formatDate(date) {
